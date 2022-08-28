368a6c08e9ec944ab7ea300109389542aa5deeba
NakagawaYuuto <github.nakagawa@gmail.com>
2022-08-28 23:45:29 +0900

    複数クラスを用いた開発

---
+++ Main.java
@@ -0,0 +1,15 @@
+import comment.Kouhan;
+import comment.Zenhan;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Zenhan.doWarusa();
+        Zenhan.doTogame();
+        Kouhan.callDeae();
+        Kouhan.showMondokoro();
+    }
+
+
+
+
+}
---
+++ comment/Zenhan.java
@@ -0,0 +1,12 @@
+package comment;
+
+public class Zenhan {
+    public static void doWarusa(){
+        System.out.println("きなこでござるか。食えませんがの。");
+    }
+
+
+    public static void doTogame(){
+        System.out.println("この老いぼれの目はごまかせませんぞ。");
+    }
+}
